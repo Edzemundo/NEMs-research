3c1ebda0f7ed4777744599cc511d1e4d002cddfb
Edzemundo <85126926+Edzemundo@users.noreply.github.com>
2022-07-07 21:46:01 -0400

    added scanning in one direction as scan2

--- MDT_scan/scan.py
+++ MDT_scan/scan.py
@@ -84,6 +84,47 @@ def scan(startz, starty, stopz, stopy, step):
     print(f"time = {total_time} seconds")
 
 
+def scan2(startz, starty, stopz, stopy, step):
+
+    global filename
+    filename = input("filename: ")
+    
+    start_time = time.time()
+    zvoltage = startz
+    yvoltage = starty
+    time.sleep(0.5)
+    stepz = int(((stopz - startz) / step))
+    stepy = int(((stopy - starty) / step))
+    # print(f"stepy = {stepy}")
+    for y in range(stepy + 1):
+        setY(yvoltage)
+        print(f"y = {yvoltage}")
+        time.sleep(0.1)
+        
+        for up in range(0, stepz + 1):
+            setZ(zvoltage)
+            print(f"z = {zvoltage}")
+            time.sleep(0.1)
+            measured_v = measure_dcv()
+            chart(zvoltage, yvoltage, measured_v)
+            zvoltage += step
+        
+        if yvoltage <= stopy:
+            yvoltage += step
+            zvoltage = startz
+            setZ(zvoltage)
+            setY(yvoltage)
+            print(f"z = {zvoltage}")
+            time.sleep(0.3)
+            # measured_v = measure_dcv()
+            # chart(zvoltage, yvoltage, measured_v)
+
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"time = {total_time} seconds")
+
+
 def chart(x_value, y_value, z_value):
     global file
     file = open(f"{filename}", "a")
--- MDT_scan/test.py
+++ MDT_scan/test.py
@@ -9,18 +9,16 @@ step = 1
 start_time = time.time()
 zvoltage = startz
 yvoltage = starty
-# setZ(startz)
-# setY(starty)
 time.sleep(0.5)
 stepz = int(((stopz - startz) / step))
 stepy = int(((stopy - starty) / step))
 # print(f"stepy = {stepy}")
-for y in range(stepy):
+for y in range(stepy + 1):
     # setY(yvoltage)
     print(f"y = {yvoltage}")
     time.sleep(0.1)
     
-    for up in range(0, stepz):
+    for up in range(0, stepz + 1):
         # setZ(zvoltage)
         print(f"z = {zvoltage}")
         time.sleep(0.1)
@@ -28,27 +26,14 @@ for y in range(stepy):
         # chart(zvoltage, yvoltage, measured_v)
         zvoltage += step
     yvoltage += step
+    zvoltage = startz
     # setZ(zvoltage)
     # setY(yvoltage)
     print(f"z = {zvoltage}")
-    print(f"y = {yvoltage}")
     time.sleep(0.1)
     # measured_v = measure_dcv()
     # chart(zvoltage, yvoltage, measured_v)
 
-    for down in range(0, stepz):
-        # setZ(zvoltage)
-        print(f"z = {zvoltage}")
-        time.sleep(0.1)
-        # measured_v = measure_dcv()
-        # chart(zvoltage, yvoltage, measured_v)
-        zvoltage -= step
-    yvoltage += step
-    # setZ(zvoltage)
-    print(f"z = {zvoltage}")
-    time.sleep(0.1)
-    # measured_v = measure_dcv()
-    # chart(zvoltage, yvoltage, measured_v)
 
 end_time = time.time()
 total_time = end_time - start_time
